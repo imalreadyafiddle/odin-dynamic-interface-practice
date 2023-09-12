7c41e99b1bf461372a3764ae2abbb3dd7f63e517
Christopher Davis <christopher.m.davis90@gmail.com>
2023-09-12 14:59:24 -0400

    skeleton of hamburger styled menu complete, needs js wiring still

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,5 +1,16 @@
 // make hamburger menu, niceniceniceniceniceniiiiice!!!
 
 function makeHamburgerMenu() {
-  //
+  // wire up the hamburger menu with click events
+}
+
+function openCloseHamburger(isOpen) {
+  // close hamburger if open
+  // open hamburger if closed
+}
+
+function addRemoveToppings(isOpen) {
+  // identify which topping was tapped
+  // close topping if open
+  // open topping if closed
 }
